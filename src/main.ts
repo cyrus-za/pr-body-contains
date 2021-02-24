7654b1efa0378fb33f196004475f34a56d7523aa
Pieter Venter <pieter@venter.pro>
2021-02-24 14:13:04 +0200

    fix: remove unused verbs

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-const repoTokenInput = core.getInput("repo-token", { required: true });
-const githubClient = github.getOctokit(repoTokenInput);
-
 async function run(): Promise<void> {
-  
   const containsString: string = core.getInput("containsString");
   const body: string =
-    (githubContext.payload.pull_request?.body as string) ?? "";
+    (github.context.payload.pull_request?.body as string) ?? "";
 
   core.debug(`PR Body: ${body}`);
-    if (!body.includes(containsString)) {
-      core.setFailed(`PR body does not contain ${containsString}`);
-   }
+  if (!body.includes(containsString)) {
+    core.setFailed(`PR body does not contain ${containsString}`);
+  }
 }
 
 run().catch((error) => {
